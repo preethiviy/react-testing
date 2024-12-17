aaa222f3e24c1319adebc94eeed8185add4a9861
preethiviy <preethivirajyambem@gmail.com>
2024-12-17 11:27:25 +0530

    order phases test

--- sundae-ui/src/pages/confirmation/OrderConfirmation.jsx
+++ sundae-ui/src/pages/confirmation/OrderConfirmation.jsx
@@ -5,54 +5,54 @@ import { useOrderDetails } from "../../contexts/OrderDetails";
 import AlertBanner from "../common/AlertBanner";
 
 export default function OrderConfirmation({ setOrderPhase }) {
-  const { resetOrder } = useOrderDetails();
-  const [orderNumber, setOrderNumber] = useState(null);
-  const [error, setError] = useState(false);
+    const { resetOrder } = useOrderDetails();
+    const [orderNumber, setOrderNumber] = useState(null);
+    const [error, setError] = useState(false);
 
-  useEffect(() => {
-    axios
-      // in a real app we would get order details from context
-      // and send with POST
-      .post(`http://localhost:3030/order`)
-      .then((response) => {
-        setOrderNumber(response.data.orderNumber);
-      })
-      .catch(() => setError(true));
-  }, []);
+    useEffect(() => {
+        axios
+            // in a real app we would get order details from context
+            // and send with POST
+            .post(`http://localhost:3030/order`)
+            .then((response) => {
+                setOrderNumber(response.data.orderNumber);
+            })
+            .catch(() => setError(true));
+    }, []);
 
-  function handleClick() {
-    // clear the order details
-    resetOrder();
+    function handleClick() {
+        // clear the order details
+        resetOrder();
 
-    // send back to order page
-    setOrderPhase("inProgress");
-  }
+        // send back to order page
+        setOrderPhase("inProgress");
+    }
 
-  const newOrderButton = (
-    <Button onClick={handleClick}>Create new order</Button>
-  );
-
-  if (error) {
-    return (
-      <>
-        <AlertBanner message={null} variant={null} />
-        {newOrderButton}
-      </>
+    const newOrderButton = (
+        <Button onClick={handleClick}>Create new order</Button>
     );
-  }
 
-  if (orderNumber) {
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Thank You!</h1>
-        <p>Your order number is {orderNumber}</p>
-        <p style={{ fontSize: "25%" }}>
-          as per our terms and conditions, nothing will happen now
-        </p>
-        {newOrderButton}
-      </div>
-    );
-  } else {
-    return <div>Loading</div>;
-  }
+    if (error) {
+        return (
+            <>
+                <AlertBanner message={null} variant={null} />
+                {newOrderButton}
+            </>
+        );
+    }
+
+    if (orderNumber) {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <h1>Thank You!</h1>
+                <p>Your order number is {orderNumber}</p>
+                <p style={{ fontSize: "25%" }}>
+                    as per our terms and conditions, nothing will happen now
+                </p>
+                {newOrderButton}
+            </div>
+        );
+    } else {
+        return <div>Loading</div>;
+    }
 }
